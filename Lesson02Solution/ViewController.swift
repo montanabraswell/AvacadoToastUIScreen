2368bcd77a58c7897e13de957d885e7fdf73c144
Montana <montana@Montanas-MacBook-Pro.local>
2021-02-28 01:22:04 -0500

    Adding code

---
+++ Lesson02Solution/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson02Solution
+//
+//  Created by Montana  on 2/27/21.
+//  Copyright © 2021 Montana Braswell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
